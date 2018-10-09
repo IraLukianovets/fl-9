754faaf23f440d8349471c89ffad4524206a0b27
Iryna Lukianovets <41140625+IraLukianovets@users.noreply.github.com>
2018-10-09 22:45:50 +0300

    fixed module file mess

--- FL_9_5_homework_es-next/homework/tower-of-babel/modules_default_export_math.js
+++ FL_9_5_homework_es-next/homework/tower-of-babel/modules_default_export_math.js
@@ -1,11 +1,17 @@
-export default PI = 3.141592;
+const PI = 3.141592;
 
-let _sqrt = function(s, x, last){
+var _sqrt = function(s, x, last){
   return x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
 };
-export default sqrt = function(s){
+const sqrt = function(s){
   return _sqrt(s, s/2.0, 0.0);
 };
-export default square = function(x){
+const square = function(x){
   return x * x;
+};
+
+export default{
+  PI: PI,
+  sqrt: sqrt,
+  square: square
 };

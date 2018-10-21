c3cf8f9b621277afcec1e98c1cdefe3762abefd5
Iryna Lukianovets <41140625+IraLukianovets@users.noreply.github.com>
2018-10-21 21:12:49 +0300

    deleted comments from middleware

--- FL_9_8_homework_nodejs-expressjs/homework/src/middlewares/delete-authorization.js
+++ FL_9_8_homework_nodejs-expressjs/homework/src/middlewares/delete-authorization.js
@@ -1,21 +1,3 @@
-const Joi = require('joi');
-/*
-function validation() {
-    const schema = {
-        brand: Joi.string().required(),
-        model: Joi.string().required(),
-        engineVolume: Joi.string().required(),
-        year: Joi.string().required(),
-
-    };
-
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        return res.status(400).send(result.error);
-    }
-}
-*/
-
 exports.checkAuthorization = (req, res, next) => {
 
     if (req.headers.authorization !== 'X-Password qwerty') {
